d78ba8312786d4258afc333a0af7fc6f4bc5ea4e
Siavash <47079861+itzreqle@users.noreply.github.com>
2023-09-03 02:49:06 +0330

    Create downloader.py

---
+++ downloader.py
@@ -0,0 +1,30 @@
+import argparse
+import requests
+import os
+
+def download_public_repositories(username):
+    base_url = f'https://api.github.com/users/{username}/repos'
+
+    response = requests.get(base_url)
+
+    if response.status_code == 200:
+        repositories = response.json()
+        for repo in repositories:
+            repo_name = repo['name']
+            repo_url = repo['html_url']
+            repo_clone_url = repo['clone_url']
+
+            # Clone the repository using Git
+            os.system(f'git clone {repo_clone_url}')
+
+            print(f"Cloned {repo_name} from {repo_url}")
+    else:
+        print(f"Failed to fetch repositories. Status code: {response.status_code}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Download public GitHub repositories for a user.')
+    parser.add_argument('username', type=str, help='GitHub username')
+
+    args = parser.parse_args()
+
+    download_public_repositories(args.username)
